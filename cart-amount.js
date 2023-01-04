9f7055e487b5f6a157c1e373030dddf07fdcf99a
Sebastian Beuerlein <basti.beuerlein@web.de>
2023-01-04 11:46:57 +0100

    Ready for dynamic Item list

--- cart-amount.js
+++ cart-amount.js
@@ -1,4 +1,11 @@
-var count = 0;
+var countItems = 0;
 
 var btnToCheckout = document.getElementById("proceed-to-checkout");
 var btnToItems = document.getElementById("back-to-itempage");
+
+
+function displayItems(countItems){
+    if (countItems == 0){
+        document.getElementById("infobox").innerHTML = "Der Warenkorb ist leer!";
+    }
+}
