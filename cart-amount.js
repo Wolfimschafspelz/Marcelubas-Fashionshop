181cd114acc19c6167fb782d624392177afb2a1c
Sebastian Beuerlein <basti.beuerlein@web.de>
2023-01-06 08:21:00 +0100

    arrays for shopping cart table

--- cart-amount.js
+++ cart-amount.js
@@ -3,6 +3,12 @@ var countItems = 0;
 var btnToCheckout = document.getElementById("proceed-to-checkout");
 var btnToItems = document.getElementById("back-to-itempage");
 
+var img_src = ["./jacke.png", "./jacke_blau.png", "./jacke_gruen.png"];
+var descript = ["Modische Herrenjacke in rot. Sowohl als Arbeitskleidung, als auch chic in der Freizeit tragbar." , 
+                "Modische Herrenjacke in blau. Sowohl als Arbeitskleidung, als auch chic in der Freizeit tragbar.", 
+                "Modische Herrenjacke in grün. Sowohl als Arbeitskleidung, als auch chic in der Freizeit tragbar."];
+var price_prod = [289.99, 279.99, 350.00];
+
 
 function displayItems(countItems){
     if (countItems == 0){
